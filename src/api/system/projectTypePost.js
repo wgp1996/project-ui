2013ea2e5cd81b9bf6cf842f3d331340d7788bdd
wgp1996 <19961221wgp>
2021-01-25 10:17:34 +0800

    提交

---
+++ src/api/system/projectTypePost.js
@@ -0,0 +1,64 @@
+import request from '@/utils/request'
+
+// 查询岗位列表
+export function listProjectType(query) {
+  return request({
+    url: '/system/projectTypePost/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询岗位树
+export function projectTypeTree(query) {
+  return request({
+    url: '/system/projectTypePost/treeselect',
+    method: 'get',
+    params: query
+  })
+}
+
+
+
+// 查询岗位详细
+export function getProjectType(projectTypeId) {
+  return request({
+    url: '/system/projectTypePost/' + projectTypeId,
+    method: 'get'
+  })
+}
+
+// 新增岗位
+export function addProjectType(data) {
+  return request({
+    url: '/system/projectTypePost',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改岗位
+export function updateProjectType(data) {
+  return request({
+    url: '/system/projectTypePost',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除岗位
+export function delProjectType(projectTypeId) {
+  return request({
+    url: '/system/projectTypePost/' + projectTypeId,
+    method: 'delete'
+  })
+}
+
+// 导出岗位
+export function exportProjectType(query) {
+  return request({
+    url: '/system/projectTypePost/export',
+    method: 'get',
+    params: query
+  })
+}
