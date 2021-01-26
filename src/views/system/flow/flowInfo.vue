5e5bc43a277adf6dbd1af11b6d491166b341edf5
yu <2502436081@qq.com>
2021-01-26 13:38:29 +0800

    提交

--- src/views/system/flow/flowInfo.vue
+++ src/views/system/flow/flowInfo.vue
@@ -56,7 +56,7 @@
           icon="el-icon-edit"
           size="mini"
           :disabled="multiple"
-          @click="handleEffect"
+           @click="handleEffect"
           v-hasPermi="['system:flowInfo:effect']"
         >启用</el-button>
       </el-col>
@@ -92,7 +92,7 @@
           >
             <el-table-column label="节点编码" align="center" prop="nodeNo" />
             <el-table-column label="顺序" align="center" prop="nodeNum" />
-            <el-table-column label="是否角色" align="center" prop="isPrName" />
+            <el-table-column label="是否角色" align="center" prop="isRoleName" />
             <el-table-column label="人员/角色名称" align="center" prop="prName" />
             <el-table-column label="是否允许结束" align="center" prop="isEndName" />
             <el-table-column label="备注" align="center" prop="remark" />
@@ -132,7 +132,9 @@
     />
 
     <!-- 添加或修改流程表对话框 -->
-    <el-dialog :title="title" :visible.sync="open" width="500px">
+    <el-dialog :title="title" :visible.sync="open" width="700px">
+       <el-tabs v-model="activeName" >
+        <el-tab-pane label="基础信息" name="first">
       <el-form ref="form" :model="form" :rules="rules" label-width="80px">
         <el-form-item label="流程名称" prop="flowName">
           <el-input v-model="form.flowName" placeholder="请输入流程名称" />
@@ -146,6 +148,110 @@
           </el-radio-group>
         </el-form-item> -->
       </el-form>
+      </el-tab-pane>
+      <el-tab-pane label="节点明细" name="second">
+          <el-row :gutter="10" class="mb8">
+            <el-col :span="1.5">
+              <el-button type="primary" icon="el-icon-plus" size="mini" @click="goodsSelect">新增</el-button>
+            </el-col>
+          </el-row>
+            <el-table
+            :data="tableData"
+            class="tb-edit"
+            style="width: 100%"
+            highlight-current-row
+             :header-cell-class-name="starAdd"
+          >
+           
+            <el-table-column  label="是否角色" width="150">
+              <template scope="scope">
+                 <el-select v-model="scope.row.isRole" placeholder="请选择是否角色" style="width:100%"
+                  @change="choose(scope.$index, scope.row)"
+                 >
+                    <el-option
+                      v-for="dict in dwOptions"
+                      :key="dict.value"
+                      :label="dict.label"
+                      :value="dict.value"
+                    ></el-option>
+                  </el-select>
+                   <!-- <span>{{scope.row.isRoleName}}</span> -->
+              </template>
+            </el-table-column>
+              <el-table-column label="选择角色" width="200" >
+                  <template scope="scope">
+                <el-select v-model="scope.row.prId" placeholder="请选择角色" style="width:100%"
+                @change="selecter(scope.$index, scope.row)" :disabled="scope.row.one"
+               >
+                    <el-option
+                      v-for="dict in roleList"
+                      :key="dict.roleKey"
+                      :label="dict.roleName"
+                      :value="dict.roleKey"
+                    ></el-option>
+                  </el-select>
+                  </template>
+            </el-table-column>
+             <el-table-column  label="选择人员" width="120" >
+              <template scope="scope">
+                <!-- <el-input
+                  :disabled="true"
+                  size="small"
+                  v-model="scope.row.prId"
+                  placeholder=""
+                ></el-input> -->
+                <el-select v-model="scope.row.prId" placeholder="请选择人员" style="width:100%" 
+                 @change="selectStore(scope.$index, scope.row)"  :disabled="scope.row.two"   >
+                    <el-option
+                      v-for="dict in people"
+                      :key="dict.nickName"
+                      :label="dict.userName"
+                      :value="dict.nickName"
+                    ></el-option>
+                  </el-select>
+                
+              </template>
+            </el-table-column>
+      
+         
+            <el-table-column label="备注说明" width="120">
+              <template scope="scope">
+                <el-input
+                  size="small"
+                  v-model="scope.row.remark"
+                  placeholder="请输入备注"
+              
+                ></el-input>
+               
+              </template>
+            </el-table-column>
+            <!-- :onkeyup="scope.row.isEnd=scope.row.isEnd.replace(/[^\d.]/g,'')" -->
+            <el-table-column label="是否允许结束" width="120">
+              <template scope="scope">
+                 <el-select v-model="scope.row.isEnd" placeholder="请选择是否允许结束" style="width:100%"  >
+                    <el-option
+                      v-for="dict in end"
+                      :key="dict.value"
+                      :label="dict.label"
+                      :value="dict.value"
+                    ></el-option>
+                  </el-select>
+              </template>
+            </el-table-column>
+          
+         
+            <el-table-column label="操作">
+              <template scope="scope">
+                <el-button
+                  size="small"
+                  type="danger"
+                  @click="handleChildDelete(scope.$index, scope.row)"
+                >删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+       </el-tabs>   
       <div slot="footer" class="dialog-footer">
         <el-button type="primary" @click="submitForm">确 定</el-button>
         <el-button @click="cancel">取 消</el-button>
@@ -155,8 +261,9 @@
 </template>
 
 <script>
-import { listFlowInfo, getFlowInfo, delFlowInfo, addFlowInfo, updateFlowInfo, exportFlowInfo,effectFlowInfo } from "@/api/system/flowInfo";
-
+import { listFlowInfo, getFlowInfo, delFlowInfo, addFlowInfo, updateFlowInfo, exportFlowInfo,effectFlowInfo,listNode,delFlowNode} from "@/api/system/flowInfo";
+import { listRole, getRole, delRole, addRole, updateRole, exportRole, dataScope, changeRoleStatus } from "@/api/system/role";
+import { listUser } from "@/api/system/user";
 export default {
   name: "FlowInfo",
   data() {
@@ -173,10 +280,31 @@ export default {
       total: 0,
       // 流程表表格数据
       flowInfoList: [],
+      // 数据
+      tableData:[],
       // 弹出层标题
       title: "",
       // 是否显示弹出层
       open: false,
+      activeName: "first",
+      one:false,
+      two:false,
+      //  是否明细
+       dwOptions:[{
+          value: '0',
+          label: '否'
+        }, {
+          value: '1',
+          label: '是'
+        }],
+        // 是否结束
+        end:[{
+          value: '0',
+          label: '否'
+        }, {
+          value: '1',
+          label: '是'
+        }],
       // 查询参数
       queryParams: {
         pageNum: 1,
@@ -189,6 +317,15 @@ export default {
       form: {},
       // 表单校验
       rules: {
+        //     isRole: [
+        //   { required: true, message: "是否角色不能为空", trigger: "blur" },
+        // ],
+        //    prId: [
+        //   { required: true, message: "人员或角色不能为空", trigger: "blur" },
+        // ],
+        //   isEnd: [
+        //   { required: true, message: "是否允许结束不能为空", trigger: "blur" },
+        // ],
       }
     };
   },
@@ -196,19 +333,84 @@ export default {
     this.getList();
   },
   methods: {
+     //追加子表必填样式
+    starAdd(obj) {
+      if(obj.columnIndex === 0 || obj.columnIndex === 1|| obj.columnIndex === 2|| obj.columnIndex === 4) {
+          return 'star';
+      }
+    },
     /** 查询流程表列表 */
     getList() {
       this.loading = true;
       listFlowInfo(this.queryParams).then(response => {
+        console.log(response)
         this.flowInfoList = response.rows;
         this.total = response.total;
         this.loading = false;
       });
+      listRole(this.queryParams).then(response => {
+          console.log(response)
+           this.roleList = response.rows;
+      
+        }
+      );
+      listUser(this.queryParams).then((response) => {
+        console.log(response);
+        this.people = response.rows;
+      });
+    },
+    choose(index,row){
+      for (let i = 0; i < this.dwOptions.length; i++) {
+           console.log(row)
+         console.log(this.dwOptions[i])
+        if (this.dwOptions[i].value == row.isRole=="1") {
+     
+           row.one = false;
+           row.two=true;
+       
+        }else if(this.dwOptions[i].value == row.isRole=="0"){
+            row.one = true;
+            row.two=false;
+        }
+      }
+ 
+    },
+    // 角色
+    selecter(index,row){
+       for (let i = 0; i < this.roleList.length; i++) {
+           console.log(row.prId)
+           console.log(this.roleList[i])
+        if (this.roleList[i].roleId == row.prId) {
+     
+           row.prName = this.roleList[i].roleName;
+          break;
+        }
+      }
+    },
+    // 选择人员
+    selectStore(index,row) {
+      for (let i = 0; i < this.people.length; i++) {
+        if (this.people[i].nickName == row.prId) {
+          console.log(this.people[i].nickName)
+           row.prName = this.people[i].userName;
+          break;
+        }
+      }
     },
     // 取消按钮
     cancel() {
       this.open = false;
       this.reset();
+    },
+    // 子表删除
+       handleChildDelete(index, row) {
+      if (row.id != "" && row.id != undefined && row.id != null) {
+        delFlowNode(row.id);
+        this.tableData.splice(index, 1);
+      } else {
+        this.tableData.splice(index, 1);
+      }
+      console.log(index, row);
     },
     // 表单重置
     reset() {
@@ -221,9 +423,16 @@ export default {
         updateBy: undefined,
         updateTime: undefined,
         remark: undefined,
-        status: "0"
+        status: "0",
+        prName:undefined,
+        prId:undefined,
+        isRole:undefined,
+        isEnd:undefined,
+        rows: "",
+        flowNo:undefined
       };
       this.resetForm("form");
+      this.tableData = [];
     },
     /** 搜索按钮操作 */
     handleQuery() {
@@ -234,6 +443,21 @@ export default {
     resetQuery() {
       this.resetForm("queryForm");
       this.handleQuery();
+    },
+    // 新增节点明细
+    goodsSelect(){
+      this.$nextTick(() => {
+         let goodsInfo = new Object();
+        goodsInfo.isRole = '';
+        goodsInfo.prId = '';
+        goodsInfo.isEnd = "";
+        goodsInfo.prName = '';
+        goodsInfo.remark = "";
+        goodsInfo.one = false;
+        goodsInfo.two = false;
+        this.tableData.push(goodsInfo);
+ 
+      });
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
@@ -255,35 +479,71 @@ export default {
         this.form = response.data;
         this.open = true;
         this.title = "修改流程表";
+        listNode(this.form.flowNo).then(response=>{
+           this.tableData = response.rows;
+           console.log( this.tableData)
+          for(var i=0;i<this.tableData.length;i++){
+            this.tableData[i].isRole=this.tableData[i].isRole+"";
+            this.tableData[i].isEnd=this.tableData[i].isEnd+"";
+            if(this.tableData[i].isRole==1){
+               this.tableData[i].one=true;
+               this.tableData[i].two=false;
+               
+            }else{
+              this.tableData[i].one=false;
+              this.tableData[i].two=true;
+              console.log(this.tableData)
+            }
+
+          }
+        })
       });
+ 
     },
     /** 提交按钮 */
     submitForm: function() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          if (this.form.id != undefined) {
-            updateFlowInfo(this.form).then(response => {
-              if (response.code === 200) {
-                this.msgSuccess("修改成功");
-                this.open = false;
-                this.getList();
-              } else {
-                this.msgError(response.msg);
+       if (this.tableData.length > 0) {
+         for( var i=0;i<this.tableData.length;i++){
+           if(
+             this.tableData[i].isRole == "" ||
+             this.tableData[i].isEnd == "" ||
+             this.tableData[i].prId == ""
+           ){
+              this.msgError("检查明细信息必填项!");
+              return;
+           }
+         }
+             this.form.rows = JSON.stringify(this.tableData);
+            this.$refs["form"].validate(valid => {
+              if (valid) {
+                if (this.form.id != undefined) {
+                  updateFlowInfo(this.form).then(response => {
+                    if (response.code === 200) {
+                      this.msgSuccess("修改成功");
+                      this.open = false;
+                      this.getList();
+                    } else {
+                      this.msgError(response.msg);
+                    }
+                  });
+                } else {
+                  addFlowInfo(this.form).then(response => {
+                    if (response.code === 200) {
+                      this.msgSuccess("新增成功");
+                      this.open = false;
+                      this.getList();
+                    } else {
+                      this.msgError(response.msg);
+                    }
+                  });
+                }
               }
             });
-          } else {
-            addFlowInfo(this.form).then(response => {
-              if (response.code === 200) {
-                this.msgSuccess("新增成功");
-                this.open = false;
-                this.getList();
-              } else {
-                this.msgError(response.msg);
-              }
-            });
-          }
-        }
-      });
+
+       }else{
+            this.msgError("请填写子表信息!");
+       }
+   
     },
     /** 删除按钮操作 */
     handleDelete(row) {
@@ -300,7 +560,7 @@ export default {
         }).catch(function() {});
     },
     /** 生效按钮操作 */
-    handleDelete(row) {
+    handleEffect(row) {
       const ids = row.id || this.ids;
       this.$confirm('是否确认启用流程表编号为"' + ids + '"的数据项?', "警告", {
           confirmButtonText: "确定",
@@ -329,3 +589,9 @@ export default {
   }
 };
 </script>
+<style>
+table th.star div::after {
+  content: "*";
+  color: red;
+}
+</style>>
