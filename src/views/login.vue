ad27351f3c0c796717f7314b6de18dd6b915de98
yu <2502436081@qq.com>
2021-01-28 11:33:24 +0800

    提交

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="login">
+        <canvas
+        id='myCanvas'
+        :width='width'
+        :height='height'
+      >
+      </canvas>
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form">
       <h3 class="title">项目管理平台</h3>
       <el-form-item prop="username">
@@ -54,8 +60,10 @@
 </template>
 
 <script>
+
 import { getCodeImg } from "@/api/login";
 import Cookies from "js-cookie";
+
 import { encrypt, decrypt } from '@/utils/jsencrypt'
 
 export default {
@@ -81,7 +89,56 @@ export default {
         code: [{ required: true, trigger: "change", message: "验证码不能为空" }]
       },
       loading: false,
-      redirect: undefined
+      redirect: undefined,
+      // 新加的
+       canvas: null,
+      context: null,
+      stars: [], //星星数组
+      shadowColorList: [
+        "#39f",
+        "#ec5707",
+        "#b031d4",
+        "#22e6c7",
+        "#92d819",
+        "#14d7f1",
+        "#e23c66"
+      ], //阴影颜色列表
+      directionList: ["leftTop", "leftBottom", "rightTop", "rightBottom"], //星星运行方向
+      speed: 30, //星星运行速度
+      last_star_created_time: new Date(), //上次重绘星星时间
+      Ball: class Ball {
+        constructor(radius) {
+          this.x = 0;
+          this.y = 0;
+          this.radius = radius;
+          this.color = "";
+          this.shadowColor = "";
+          this.direction = "";
+        }
+
+        draw(context) {
+          context.save();
+          context.translate(this.x, this.y);
+          context.lineWidth = this.lineWidth;
+          var my_gradient = context.createLinearGradient(0, 0, 0, 8);
+          my_gradient.addColorStop(0, this.color);
+          my_gradient.addColorStop(1, this.shadowColor);
+          context.fillStyle = my_gradient;
+          context.beginPath();
+          context.arc(0, 0, this.radius, 0, Math.PI * 2, true);
+          context.closePath();
+
+          context.shadowColor = this.shadowColor;
+          context.shadowOffsetX = 0;
+          context.shadowOffsetY = 0;
+          context.shadowBlur = 10;
+
+          context.fill();
+          context.restore();
+        }
+      }, //工厂模式定义Ball类
+      width: window.innerWidth,
+      height: window.innerHeight,
     };
   },
   beforeCreate(){
@@ -96,11 +153,104 @@ export default {
     }
   },
   created() {
-   
+    // colorValue(),
+  //  createColorStyle(),
+  //  mixComponents(), 
+  //   averageColorStyles(),
+  //    Color(),
+  //     Dot(),
+  //    createDots(),
+  //     moveDots(), 
+  //     connectDots(),
+  //      drawDots(),
+  //       animateDots(),
     this.getCode();
     this.getCookie();
   },
   methods: {
+       //重复动画
+    drawFrame() {
+      let animation = requestAnimationFrame(this.drawFrame);
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      this.createStar(false);
+      this.stars.forEach(this.moveStar);
+    },
+        //展示所有的星星
+    createStar(params) {
+      let now = new Date();
+      if (params) {
+        //初始化星星
+        for (var i = 0; i <80; i++) {
+          const radius = Math.random() * 1 + 2;
+          let star = new this.Ball(radius);
+          star.x = Math.random() * this.canvas.width + 1;
+          star.y = Math.random() * this.canvas.height + 1;
+          star.color = "#ffffff";
+          star.shadowColor = this.shadowColorList[
+            Math.floor(Math.random() * this.shadowColorList.length)
+          ];
+          star.direction = this.directionList[
+            Math.floor(Math.random() * this.directionList.length)
+          ];
+          this.stars.push(star);
+        }
+      } else if (!params && now - this.last_star_created_time > 3000) {
+        //每隔3秒重绘修改颜色其中30个球阴影颜色
+        for (var i = 0; i < 30; i++) {
+          this.stars[i].shadowColor = this.shadowColorList[
+            Math.floor(Math.random() * this.shadowColorList.length)
+          ];
+        }
+        this.last_star_created_time = now;
+      }
+    },
+        //移动
+    moveStar(star, index) {
+      if (star.y - this.canvas.height > 0) {
+        //触底
+        if (Math.floor(Math.random() * 2) === 1) {
+          star.direction = "leftTop";
+        } else {
+          star.direction = "rightTop";
+        }
+      } else if (star.y < 2) {
+        //触顶
+        if (Math.floor(Math.random() * 2) === 1) {
+          star.direction = "rightBottom";
+        } else {
+          star.direction = "leftBottom";
+        }
+      } else if (star.x < 2) {
+        //左边
+        if (Math.floor(Math.random() * 2) === 1) {
+          star.direction = "rightTop";
+        } else {
+          star.direction = "rightBottom";
+        }
+      } else if (star.x - this.canvas.width > 0) {
+        //右边
+        if (Math.floor(Math.random() * 2) === 1) {
+          star.direction = "leftBottom";
+        } else {
+          star.direction = "leftTop";
+        }
+      }
+      if (star.direction === "leftTop") {
+        star.y -= star.radius / this.speed;
+        star.x -= star.radius / this.speed;
+      } else if (star.direction === "rightBottom") {
+        star.y += star.radius / this.speed;
+        star.x += star.radius / this.speed;
+      } else if (star.direction === "leftBottom") {
+        star.y += star.radius / this.speed;
+        star.x -= star.radius / this.speed;
+      } else if (star.direction === "rightTop") {
+        star.y -= star.radius / this.speed;
+        star.x += star.radius / this.speed;
+      }
+      star.draw(this.context);
+    },
+  
     getCode() {
       getCodeImg().then(res => {
         this.codeUrl = "data:image/gif;base64," + res.img;
@@ -143,6 +293,13 @@ export default {
         }
       });
     }
+  },
+    mounted() {
+    this.canvas = document.getElementById("myCanvas");
+    this.context = this.canvas.getContext("2d");
+
+    this.createStar(true);
+    this.drawFrame();
   }
 };
 </script>
@@ -153,7 +310,7 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/image/bj.jpeg");
+  background-image: url("../assets/image/bg1.jpg");
   background-size: cover;
 }
 .title {
@@ -164,9 +321,20 @@ export default {
 
 .login-form {
   border-radius: 6px;
-  background: #ffffff;
+  background: rgba(0,0,0,.5);
   width: 400px;
   padding: 25px 25px 5px 25px;
+  width: 540px;
+    height: 400px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
+  
+    box-shadow: -15px 15px 15px rgba(6,17,47,.7);
+    z-index: 99999;
   .el-input {
     height: 38px;
     input {
@@ -205,4 +373,12 @@ export default {
   font-size: 12px;
   letter-spacing: 1px;
 }
+.cavs {
+    z-index: 9999;
+    position: fixed;
+    width: 95%;
+    margin-left: 20px;
+    margin-right: 20px;
+}
+.login-title{color:#d3d7f7;height:60px;font-size:20px;text-align:center;margin-top:-20px}.login-con{height:208px;position:absolute;left:0;width:80%;margin:0 10%}.login-user{position:relative}.icon{position:absolute;z-index:1;left:36px;top:8px;opacity:1}.login-con input{width:calc(100% - 130px);margin-top:-2px;background:transparent;left:0;padding:10px 65px;border-top:2px solid transparent;border-bottom:2px solid transparent;border-right:none;border-left:none;outline:none;font-family:gudea,sans-serif;box-shadow:none;color:#61bfff!important}.login-pwd,.login-yan{position:relative}.login-btn{width:80%;margin:0 auto;position:relative}.login-btn input{border-radius:3px;background:0 0;border:2px solid #4fa1d9;color:#4fa1d9;text-transform:uppercase;font-size:11px;cursor:pointer;position:absolute;top:50px;left:0;right:0;margin:auto;width:80%;transition-property:background,color;-webkit-transition-duration:.2s;transition-duration:.2s}.login-btn input:hover{color:#fff!important;background:#4fa1d9;cursor:pointer;-webkit-transition-property:background,color;transition-property:background,color;-webkit-transition-duration:.2s;transition-duration:.2s}
 </style>
