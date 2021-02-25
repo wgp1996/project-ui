656e423f2348669c66ef2cda222edc8210d643da
yu <2502436081@qq.com>
2021-02-25 18:25:07 +0800

    提交

--- src/views/system/user/profile/userInfo.vue
+++ src/views/system/user/profile/userInfo.vue
@@ -1,20 +1,40 @@
 <template>
   <el-form ref="form" :model="user" :rules="rules" label-width="80px">
-    <el-form-item label="用户昵称" prop="nickName">
-      <el-input v-model="user.nickName" />
+    <el-form-item label="用户名称" prop="userName">
+      <el-input v-model="user.userName" />
     </el-form-item> 
-    <el-form-item label="手机号码" prop="phonenumber">
-      <el-input v-model="user.phonenumber" maxlength="11" />
+      <el-form-item prop="certificatesType" label="机构类型">
+        <el-select
+          v-model="user.certificatesType"
+          placeholder="请选择机构类型"
+          style="width: 100%"
+          class="specials"
+          @change="selectType"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-form-item>
+    <el-form-item :label="certificatesCode" prop="certificatesCode">
+      <el-input v-model="user.certificatesCode" maxlength="11" />
+    </el-form-item>
+     <el-form-item :label="nickName" prop="nickName">
+      <el-input v-model="user.nickName" maxlength="11" />
     </el-form-item>
     <el-form-item label="邮箱" prop="email">
       <el-input v-model="user.email" maxlength="50" />
     </el-form-item>
-    <el-form-item label="性别">
+    <!-- <el-form-item label="性别">
       <el-radio-group v-model="user.sex">
         <el-radio label="0">男</el-radio>
         <el-radio label="1">女</el-radio>
       </el-radio-group>
-    </el-form-item>
+    </el-form-item> -->
     <el-form-item>
       <el-button type="primary" size="mini" @click="submit">保存</el-button>
       <el-button type="danger" size="mini" @click="close">关闭</el-button>
@@ -36,16 +56,25 @@ export default {
       // 表单校验
       rules: {
         nickName: [
-          { required: true, message: "用户昵称不能为空", trigger: "blur" }
+          { required: true, message: "姓名或公司名称不能为空", trigger: "blur" }
         ],
-        email: [
-          { required: true, message: "邮箱地址不能为空", trigger: "blur" },
-          {
-            type: "email",
-            message: "'请输入正确的邮箱地址",
-            trigger: ["blur", "change"]
-          }
+           certificatesCode: [
+          { required: true, message: "身份证号或信用代码不能为空", trigger: "blur" }
         ],
+        certificatesType:[
+          { required: true, message: "机构类型不能为空", trigger: "blur" }
+        ],
+        userName:[
+          { required: true, message: "用户名称不能为空", trigger: "blur" }
+        ],
+        // email: [
+        //   { required: true, message: "邮箱地址不能为空", trigger: "blur" },
+        //   {
+        //     type: "email",
+        //     message: "'请输入正确的邮箱地址",
+        //     trigger: ["blur", "change"]
+        //   }
+        // ],
         phonenumber: [
           { required: true, message: "手机号码不能为空", trigger: "blur" },
           {
@@ -54,7 +83,20 @@ export default {
             trigger: "blur"
           }
         ]
-      }
+      },
+     // 机构类型选项
+      options: [
+        {
+          value: "0",
+          label: "经营性自然人",
+        },
+        {
+          value: "1",
+          label: "机构或企业",
+        }
+      ],
+      nickName: "姓名",
+      certificatesCode: "身份证号",
     };
   },
   methods: {
@@ -70,6 +112,19 @@ export default {
           });
         }
       });
+    },
+    // 选择自然人或机构
+       selectType(data) {
+      //alert(data)
+      if (data == 0) {
+        this.nickName = "姓名";
+        this.certificatesCode = "身份证号";
+        // this.user.picture = "身份证照片:";
+      } else {
+        this.nickName = "公司名称";
+        this.certificatesCode = "信用代码";
+        // this.user.picture = "营业执照:";
+      }
     },
     close() {
       this.$store.dispatch("tagsView/delView", this.$route);
