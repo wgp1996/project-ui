e3b053ad039ccc2f5e04df9f1e4fc0e15e709554
yu <2502436081@qq.com>
2021-02-26 10:33:31 +0800

    提交

--- src/views/system/user/profile/index.vue
+++ src/views/system/user/profile/index.vue
@@ -13,24 +13,24 @@
             <ul class="list-group list-group-striped">
               <li class="list-group-item">
                 <svg-icon icon-class="user" />用户名称
-                <div class="pull-right">{{ user.userName }}</div>
+                <div class="pull-right">{{ user.nickName }}</div>
               </li>
               <li class="list-group-item">
                 <svg-icon icon-class="phone" />手机号码
-                <div class="pull-right">{{ user.phonenumber }}</div>
+                <div class="pull-right">{{ user.userName }}</div>
               </li>
               <li class="list-group-item">
                 <svg-icon icon-class="email" />用户邮箱
                 <div class="pull-right">{{ user.email }}</div>
               </li>
-              <li class="list-group-item">
+              <!-- <li class="list-group-item">
                 <svg-icon icon-class="tree" />所属部门
                 <div class="pull-right" v-if="user.dept">{{ user.dept.deptName }} / {{ postGroup }}</div>
               </li>
               <li class="list-group-item">
                 <svg-icon icon-class="peoples" />所属角色
                 <div class="pull-right">{{ roleGroup }}</div>
-              </li>
+              </li> -->
               <li class="list-group-item">
                 <svg-icon icon-class="date" />创建日期
                 <div class="pull-right">{{ user.createTime }}</div>
@@ -82,7 +82,13 @@ export default {
     getUser() {
       getUserProfile().then(response => {
         this.user = response.data;
-        this.roleGroup = response.roleGroup;
+        if(this.user.createBy=="admin"){
+          this.user.show=true
+        }else{
+           this.user.show=false
+        }
+        console.log(this.user)
+       this.roleGroup = response.roleGroup;
         this.postGroup = response.postGroup;
       });
     }
--- src/views/system/user/profile/userInfo.vue
+++ src/views/system/user/profile/userInfo.vue
@@ -1,6 +1,7 @@
 <template>
   <el-form ref="form" :model="user" :rules="rules" label-width="80px">
-    <el-form-item label="用户名称" prop="userName">
+    <div v-if="user.show">
+    <el-form-item label="手机号" prop="userName">
       <el-input v-model="user.userName" />
     </el-form-item> 
       <el-form-item prop="certificatesType" label="机构类型">
@@ -29,12 +30,30 @@
     <el-form-item label="邮箱" prop="email">
       <el-input v-model="user.email" maxlength="50" />
     </el-form-item>
+    </div>
     <!-- <el-form-item label="性别">
       <el-radio-group v-model="user.sex">
         <el-radio label="0">男</el-radio>
         <el-radio label="1">女</el-radio>
       </el-radio-group>
     </el-form-item> -->
+    <div v-else>
+  <el-form-item label="业户名称" prop="nickName">
+      <el-input v-model="user.nickName" />
+    </el-form-item> 
+    <el-form-item label="手机号码" prop="phonenumber">
+      <el-input v-model="user.phonenumber" maxlength="11" />
+    </el-form-item>
+    <el-form-item label="邮箱" prop="email">
+      <el-input v-model="user.email" maxlength="50" />
+    </el-form-item>
+    <el-form-item label="性别">
+      <el-radio-group v-model="user.sex">
+        <el-radio label="0">男</el-radio>
+        <el-radio label="1">女</el-radio>
+      </el-radio-group>
+    </el-form-item>
+    </div>
     <el-form-item>
       <el-button type="primary" size="mini" @click="submit">保存</el-button>
       <el-button type="danger" size="mini" @click="close">关闭</el-button>
@@ -84,6 +103,9 @@ export default {
           }
         ]
       },
+      mounted() {
+      
+     },
      // 机构类型选项
       options: [
         {
