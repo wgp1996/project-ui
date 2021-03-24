1795e68cb131ac0e8960e1bc93435ed31a9b6bf6
wgp1996 <wgp1996>
2021-03-24 10:02:01 +0800

    更新提交

---
+++ src/api/system/projectInvent.js
@@ -0,0 +1,53 @@
+import request from '@/utils/request'
+
+// 查询编制清单列表
+export function listProjectInvent(query) {
+  return request({
+    url: '/system/projectInvent/list',
+    method: 'get',
+    params: query
+  })
+}
+
+// 查询编制清单详细
+export function getProjectInvent(id) {
+  return request({
+    url: '/system/projectInvent/' + id,
+    method: 'get'
+  })
+}
+
+// 新增编制清单
+export function addProjectInvent(data) {
+  return request({
+    url: '/system/projectInvent',
+    method: 'post',
+    data: data
+  })
+}
+
+// 修改编制清单
+export function updateProjectInvent(data) {
+  return request({
+    url: '/system/projectInvent',
+    method: 'put',
+    data: data
+  })
+}
+
+// 删除编制清单
+export function delProjectInvent(id) {
+  return request({
+    url: '/system/projectInvent/' + id,
+    method: 'delete'
+  })
+}
+
+// 导出编制清单
+export function exportProjectInvent(query) {
+  return request({
+    url: '/system/projectInvent/export',
+    method: 'get',
+    params: query
+  })
+}
